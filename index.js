c23e6187914b34217fd19f54f100f5208e4561ca
haianh-team <100552459+haianh-team@users.noreply.github.com>
2022-02-28 13:26:04 +0700

    push all

---
+++ index.js
@@ -0,0 +1,16 @@
+let count = 0;
+hello = async (items)=>{
+  	items.forEach(async()=>{
+     	await someAPICall();
+     	count++;
+  	})
+  	console.log("count = " + count);
+}
+someAPICall = ()=>{
+	return new Promise((resolve,reject)=>{
+		setTimeout(()=>{
+		resolve("done")
+		},100);
+	})
+}
+hello(['1','2','3','4']);
